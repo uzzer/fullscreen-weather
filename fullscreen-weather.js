6c7262f7ffc1b471d565f91435cac4a1f9305bd2
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-12-06 16:57:29 +0200

    update inteval 5 minutes

--- fullscreen-weather.js
+++ fullscreen-weather.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
     var key = $.parseQuery().key;
-    $.ajax({
-        url: "http://api.wunderground.com/api/" + key + "/conditions/q/autoip.json",
-        dataType: "jsonp",
-        success: function (data) {
-            $("#temp").text(data.current_observation.temp_c);
-        },
-        error: function () {
-            alert("Can't get the current weather conditions");
-        }
-    });
+    var update = function () {
+        $.ajax({
+            url: "http://api.wunderground.com/api/" + key + "/conditions/q/autoip.json",
+            dataType: "jsonp",
+            success: function (data) {
+                $("#temp").text(data.current_observation.temp_c);
+                $("#time").text(data.current_observation.observation_time);
+            },
+            error: function () {
+                alert("Can't get the current weather conditions");
+            }
+        });
+    };
+    update();
+    setInterval(update, 300000);
 });
