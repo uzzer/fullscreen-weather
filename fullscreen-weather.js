bc4a15a31bdf91b97ef7ef174736f34c18957527
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-12-06 17:50:06 +0200

    prevent often updates

--- fullscreen-weather.js
+++ fullscreen-weather.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
-    $.ajax({
-        url: "http://api.wunderground.com/api/" + $.parseQuery().key + "/conditions/q/autoip.json",
-        dataType: "jsonp",
-        success: function (data) {
-            $("#temp").text(data.current_observation.temp_c);
-            $("#time").text(data.current_observation.observation_time);
-        },
-        error: function () {
-            alert("Can't get the current weather conditions");
-        }
-    });
+    var key = "weather.time";
+    var delay = 300000;
+    var prev_time = localStorage[key];
+    var curr_time = $.now();
+    if ((typeof(prev_time) == "undefined") || (prev_time + delay < curr_time )) {
+        localStorage[key] = curr_time;
+        $.ajax({
+            url: "http://api.wunderground.com/api/" + $.parseQuery().key + "/conditions/q/autoip.json",
+            dataType: "jsonp",
+            success: function (data) {
+                $("#temp").text(data.current_observation.temp_c);
+                $("#time").text(data.current_observation.observation_time);
+            },
+            error: function () {
+                alert("Can't get the current weather conditions");
+            }
+        });
+    }
 });
