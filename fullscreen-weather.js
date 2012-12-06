d62cdfc6b68cbd9b5c5588d6da0fa7413cdd955e
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-12-06 17:05:04 +0200

    no automatic refresh

--- fullscreen-weather.js
+++ fullscreen-weather.js
@@ -1,18 +1,13 @@
 $(document).ready(function () {
-    var key = $.parseQuery().key;
-    var update = function () {
-        $.ajax({
-            url: "http://api.wunderground.com/api/" + key + "/conditions/q/autoip.json",
-            dataType: "jsonp",
-            success: function (data) {
-                $("#temp").text(data.current_observation.temp_c);
-                $("#time").text(data.current_observation.observation_time);
-            },
-            error: function () {
-                alert("Can't get the current weather conditions");
-            }
-        });
-    };
-    update();
-    setInterval(update, 300000);
+    $.ajax({
+        url: "http://api.wunderground.com/api/" + $.parseQuery().key + "/conditions/q/autoip.json",
+        dataType: "jsonp",
+        success: function (data) {
+            $("#temp").text(data.current_observation.temp_c);
+            $("#time").text(data.current_observation.observation_time);
+        },
+        error: function () {
+            alert("Can't get the current weather conditions");
+        }
+    });
 });
