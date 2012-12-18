1502143e2ef36085b9368afb92bd96e783db561b
Eduard Shustrov <eshustrov@gmail.com>
2012-12-18 03:14:08 +0200

    hourly forecast

--- fullscreen-weather.js
+++ fullscreen-weather.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
     var delay = 10 * 60 * 1000;
 
+    var weather_base = "http://api.wunderground.com/api/";
+    var weather_location = "/q/autoip.json";
+    var weather_current = "/conditions" + weather_location;
+    var weather_hourly = "/hourly" + weather_location;
+
     var key = "fullscreen-weather";
     var key_time = key + "." + "time";
-    var key_values = key + "." + "values";
+    var key_current = key + "." + "current";
+    var key_hourly = key + "." + "hourly";
 
     var status = function (message) {
         $("#status").text(message);
@@ -17,19 +23,27 @@ $(document).ready(function () {
     var then = localStorage[key_time];
     var now = $.now();
 
-    var update = function (values) {
-        $("#place").text(values.place);
-        $("#temp").text(values.temp);
-        status(values.text);
+    var update_current = function (weather) {
+        $("#place").text(weather.place);
+        $("#temp").text(weather.temp);
+        status(weather.time);
+    };
+
+    var update_hourly = function (weather) {
+        $("#hourly").children().each(function (i) {
+            $(this).find(".time").text(weather[i].time);
+            $(this).find(".temp").text(weather[i].temp);
+        });
     };
 
     if (then && (Number(then) + delay > now)) {
-        update(JSON.parse(localStorage[key_values]));
+        update_current(JSON.parse(localStorage[key_current]));
+        update_hourly(JSON.parse(localStorage[key_hourly]));
         return;
     }
 
     $.ajax({
-        url: "http://api.wunderground.com/api/" + $.parseQuery().key + "/conditions/q/autoip.json",
+        url: weather_base + $.parseQuery().key + weather_current,
         dataType: "jsonp",
         success: function (data) {
             localStorage[key_time] = now;
@@ -39,16 +53,53 @@ $(document).ready(function () {
                 return;
             }
 
-            var values = {
+            var format_time = function (epoch) {
+                var pad = function (number) {
+                    return number < 10 ? "0" + number : String(number)
+                };
+
+                var date = new Date(Number(epoch) * 1000);
+                return pad(date.getHours()) + ":" + pad(date.getMinutes());
+            };
+
+            var weather = {
                 place: data.current_observation.display_location.full,
                 temp: data.current_observation.temp_c,
-                text: data.current_observation.observation_time
+                time: "Updated at " + format_time(data.current_observation.observation_epoch)
             };
-            localStorage[key_values] = JSON.stringify(values);
-            update(values);
+            localStorage[key_current] = JSON.stringify(weather);
+            update_current(weather);
         },
         error: function () {
             status("Can't get the current weather conditions");
         }
     });
-});
+
+    $.ajax({
+        url: weather_base + $.parseQuery().key + weather_hourly,
+        dataType: "jsonp",
+        success: function (data) {
+            localStorage[key_time] = now;
+
+            if (data.response.error) {
+                status(data.response.error.description);
+                return;
+            }
+
+            var weather = [];
+            for (var i = 0; i < data.hourly_forecast.length; i++) {
+                weather[i] = {
+                    time: data.hourly_forecast[i].FCTTIME.hour_padded + ":" + data.hourly_forecast[i].FCTTIME.min,
+                    temp: data.hourly_forecast[i].temp.metric
+                }
+            }
+            localStorage[key_hourly] = JSON.stringify(weather);
+            update_hourly(weather);
+        },
+        error: function () {
+            status("Can't get hourly weather forecast");
+        }
+    })
+    ;
+})
+;
